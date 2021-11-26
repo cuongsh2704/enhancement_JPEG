e783f921b343a549e0423e8d0e69087212108aae
Dương cương <talonnoxos@gmail.com>
2021-11-26 23:01:38 +0700

    add

--- JPEG/main.py
+++ JPEG/main.py
@@ -4,7 +4,7 @@ def ShowImg(img1, img2):
     cv2.imshow("title", img1)
     cv2.imshow("title2", img2)
     cv2.waitKey(0)
-def filterD(img, x, y, size, r):
+def filterD(img, x, y, size, r): #hodges_lehman D,size : size of window , r : range , img(x,y)
     space = int(size/2)
     center = int(img[x][y][1])
     D = []
@@ -40,13 +40,39 @@ def flatImg(img):
         for j in range(0, w - 1):
             img_flat[i][j] = img[i][j][1]
     return img_flat
+def  MSM_filter(img, x, y, size):  #multi_stage_Median filter
+    space = int(size / 2)
+    W1 = 0
+    W2 = 0
+    W3 = 0
+    W4 = 0
+    for i in range(-space, space + 1):
+        W1 += img[x + i][y][1]
+        W2 += img[x][y + i][1]
+        W3 += img[x + i][y + i][1]
+        W4 += img[x - i][y - i][1]
+    W1 = W1 / size
+    W2 = W2 / size
+    W3 = W3 / size
+    W4 = W4 / size
+    Med = (max(W1, W2, W3, W4) + min(W1, W2, W3, W4) + img[x][y][1]) / 3
+    Med = int(Med)
+    img[x][y][0] = Med
+    img[x][y][1] = Med
+    img[x][y][2] = Med
+    return img
+def ShowImg(img):
+    cv2.imshow("origin", img)
+    cv2.waitKey(0)
+def AFC1(img, size): #Adaptive Filter Combination, size : size of window
+    (H, W, D) = img.shape
+    for i in range(size, H - size):
+        for j in range(size, W - size):
+            img = MSM_filter(img, i, j, size)
+    return img
+def AFC2(img, size):
+    return img
 img = cv2.imread("image.png")
-(H, W, D) = img.shape
-cv2.imshow("test1",img)
-edge = cv2.Canny(img, 100, 100)
-for i in range(5, H - 5):
-    for j in range(5, W - 5):
-        img = filterD(img, i, j, 5, 20)
-
-cv2.imshow("test", img)
-cv2.waitKey(0)
+edge = cv2.Canny(img, 256, 256)
+ShowImg(edge)
+print(edge)
