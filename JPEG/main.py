de399eac76430effadb81d8b9faacbd1c8d7d5b7
Dương cương <talonnoxos@gmail.com>
2021-12-06 23:55:01 +0700

    update

--- JPEG/main.py
+++ JPEG/main.py
@@ -4,14 +4,19 @@ def ShowImg(img1, img2):
     cv2.imshow("title", img1)
     cv2.imshow("title2", img2)
     cv2.waitKey(0)
+def flatImg(img):
+    (h, w, d) = img.shape
+    img_flat = np.zeros((h, w), dtype= int)
+    for i in range(0, h):
+        for j in range(0, w):
+            img_flat[i][j] = img[i][j][0]
+    return img_flat
 def filterD(img, x, y, size): #hodges_lehman D,size : size of window , r : range , img(x,y)
     space = int(size/2)
-    center = int(img[x][y][1])
     D = []
     for u in range(x - space, x + space + 1):
         for v in range(y - space, y + space + 1):
-            D.append(img[u][v][1])
-
+            D.append(img[u][v][0])
     D = sorted(D)
     Ave = []
     N = int(len(D) / 2)
@@ -19,26 +24,16 @@ def filterD(img, x, y, size): #hodges_lehman D,size : size of window , r : range
     for i in range(0, N):
         value = (int(D[i]) + int(D[lenD - i -2])) / 2
         Ave.append(value)
+    Med = np.mean(Ave)
+    return Med
+def median_filter(img, x, y, size):
+    arr = []
+    for i in range(x - 2, x + 2):
+        for j in range(y - 2, y + 2):
+            arr.append(img[i][j][1])
+    Med = np.mean(arr)
+    return Med
 
-    Med = Median(Ave)
-
-    img[x][y][0] = Med
-    img[x][y][1] = Med
-    img[x][y][2] = Med
-    return img
-def Median(arr):
-    sumar = sum(arr)
-    l = len(arr)
-    if (l == 0) :
-        l = 1
-    return int(sumar / l)
-def flatImg(img):
-    (h, w, d) = img.shape
-    img_flat = np.zeros((h, w), dtype= int)
-    for i in range(0, h):
-        for j in range(0, w):
-            img_flat[i][j] = img[i][j][1]
-    return img_flat
 def  MSM_filter(img, x, y, size):  #multi_stage_Median filter
     space = int(size / 2)
     W1 = 0
@@ -56,10 +51,7 @@ def  MSM_filter(img, x, y, size):  #multi_stage_Median filter
     W4 = W4 / size
     Med = (max(W1, W2, W3, W4) + min(W1, W2, W3, W4) + img[x][y][1]) / 3
     Med = int(Med)
-    img[x][y][0] = Med
-    img[x][y][1] = Med
-    img[x][y][2] = Med
-    return img
+    return Med
 
 def NZC(img, x, y): # default window size = 5 , n = 12
     nzc = 0;
@@ -105,7 +97,8 @@ def AFC1(img, size): #Adaptive Filter Combination, size : size of window
     for i in range(size, H - size):
         for j in range(size, W - size):
             if (img_classify[i][j] != 255):
-                filterD(img, i, j, 3)
+                point = filterD(img, i, j, 5)
+                img[i][j][0] = img[i][j][1] = img[i][j][2] = point
     return img
 def AFC2(img, size):
     img_classify = Classify(img)
@@ -114,33 +107,34 @@ def AFC2(img, size):
     for i in range(size, H - size):
         for j in range(size, W - size):
             if (img_classify[i][j] != 255):
-                filterD(img, i, j, 3)
+                img = filterD(img, i, j, 3)
     for i in range(size, H - size):
         for j in range(size, W - size):
             if (img_classify[i][j] != 255):
-                MSM_filter(img, i, j, 5)
+                img = MSM_filter(img, i, j, 5)
     for i in range(size, H - size):
         for j in range(size, W - size):
-            filterD(img, i, j, 3)
+            img = filterD(img, i, j, 3)
     return img
 def Evaluate(img, img_fil):
-    im = flatImg(img)
-    im_fil = flatImg(img_fil)
-
     (H, W, D) = img.shape
+    med = []
     for i in range(0, H):
         for j in range(0, W):
-            im[i][j] -= im_fil[i][j]
-            im[i][j] = np.abs(im[i][j])
+            med.append(np.abs(int(img[i][j][0]) - int(img_fil[i][j][0])))
 
-    Med = np.mean(im)
-    return Med
+    Med_eval = np.mean(med)
+    return Med_eval
 
-img = cv2.imread("anh.png")
-(H, W, D) = img.shape
-cv2.imshow("or",img)
-for i in (5, H - 5):
-    for j in (5, W - 5):
-        MSM_filter(img, i ,j ,5)
-cv2.imshow("test", img)
-cv2.waitKey(0)
+img = cv2.imread("image.png")
+img_jpeg = cv2.imread("image.jpg")
+img2 = img_jpeg
+print(Evaluate(img, img2))
+#img_fil = AFC1(img_jpeg, 5)
+print(Evaluate(img, img2))
+
+
+
+
+
+
